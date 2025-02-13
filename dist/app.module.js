58718577272f380b9ce6b3814e332ac2b5bc6641
dreammehedi <dreammehedihassan@gmail.com>
2025-02-13 09:57:11 +0600

    update

--- dist/app.module.js
+++ dist/app.module.js
@@ -8,15 +8,12 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.AppModule = void 0;
 const common_1 = require("@nestjs/common");
+const config_1 = require("@nestjs/config");
+const mongoose_1 = require("@nestjs/mongoose");
+const schedule_1 = require("@nestjs/schedule");
 const app_controller_1 = require("./app.controller");
 const app_service_1 = require("./app.service");
-const mongoose_1 = require("@nestjs/mongoose");
-const config_1 = require("@nestjs/config");
-const auth_module_1 = require("./components/auth/auth.module");
 const media_upload_module_1 = require("./components/file-management/media-upload/media-upload.module");
-const coins_module_1 = require("./components/coins/coins.module");
-const schedule_1 = require("@nestjs/schedule");
-const nft_module_1 = require("./components/nft/nft.module");
 let AppModule = class AppModule {
 };
 exports.AppModule = AppModule;
@@ -28,9 +25,6 @@ exports.AppModule = AppModule = __decorate([
             }),
             mongoose_1.MongooseModule.forRoot(process.env.MONGO_URI),
             schedule_1.ScheduleModule.forRoot(),
-            auth_module_1.AuthModule.forRoot(),
-            coins_module_1.CoinsModule,
-            nft_module_1.NftModule,
             media_upload_module_1.MediaUploadModule,
         ],
         controllers: [app_controller_1.AppController],
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,14 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ScheduleModule } from '@nestjs/schedule';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { MongooseModule } from '@nestjs/mongoose';
-import { ConfigModule } from '@nestjs/config';
-import { AuthModule } from './components/auth/auth.module';
 import { MediaUploadModule } from './components/file-management/media-upload/media-upload.module';
-import { CoinsModule } from './components/coins/coins.module';
-import { ScheduleModule } from '@nestjs/schedule';
-import { NftModule } from './components/nft/nft.module';
-import { waitForDebugger } from 'inspector';
 
 @Module({
   imports: [
@@ -17,12 +13,12 @@ import { waitForDebugger } from 'inspector';
     }),
     MongooseModule.forRoot(process.env.MONGO_URI),
     ScheduleModule.forRoot(),
-    AuthModule.forRoot(),
-    CoinsModule,
-    NftModule,
+    // AuthModule.forRoot(),
+    // CoinsModule,
+    // NftModule,
     MediaUploadModule,
   ],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule { }
+export class AppModule {}
