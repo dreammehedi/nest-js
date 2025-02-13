e7984412de56a0b254340f84bbb9a8c5749c7817
dreammehedi <dreammehedihassan@gmail.com>
2025-02-13 09:50:58 +0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,34 @@
+import { ValidationPipe } from '@nestjs/common';
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  });
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  // app.useGlobalInterceptors(new LoggingInterceptor());
+
+  const config = new DocumentBuilder()
+    .setTitle('Crypto API')
+    .setDescription('Crypto API description')
+    .setVersion('1.0')
+    .addTag('api')
+    .addBearerAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  // SwaggerModule.setup('swagger', app, document);
+
+  const port = 3000;
+  await app.listen(port, () => {
+    console.log(`API started on port ${port}`);
+  });
+}
+bootstrap();
